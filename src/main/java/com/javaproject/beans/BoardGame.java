78b516d832750e33263f81b6eb6f821a1c131ab1
vishal vishwakarma <119778329+vishal343012@users.noreply.github.com>
2026-03-13 16:15:26 +0530

    Update BoardGame.java

--- src/main/java/com/javaproject/beans/BoardGame.java
+++ src/main/java/com/javaproject/beans/BoardGame.java
@@ -4,74 +4,71 @@ import java.util.List;
 
 public class BoardGame {
 
-```
-private Long id;
-private String name;
-private int level;
-private int minPlayers;
-private int maxPlayers;
-private String gameType;
+    private Long id;
+    private String name;
+    private int level;
+    private int minPlayers;
+    private int maxPlayers;
+    private String gameType;
 
-private List<Review> reviews;
+    private List<Review> reviews;
 
-public BoardGame() {
-}
+    public BoardGame() {
+    }
 
-public Long getId() {
-    return id;
-}
+    public Long getId() {
+        return id;
+    }
 
-public void setId(Long id) {
-    this.id = id;
-}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-public String getName() {
-    return name;
-}
+    public String getName() {
+        return name;
+    }
 
-public void setName(String name) {
-    this.name = name;
-}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-public int getLevel() {
-    return level;
-}
+    public int getLevel() {
+        return level;
+    }
 
-public void setLevel(int level) {
-    this.level = level;
-}
+    public void setLevel(int level) {
+        this.level = level;
+    }
 
-public int getMinPlayers() {
-    return minPlayers;
-}
+    public int getMinPlayers() {
+        return minPlayers;
+    }
 
-public void setMinPlayers(int minPlayers) {
-    this.minPlayers = minPlayers;
-}
+    public void setMinPlayers(int minPlayers) {
+        this.minPlayers = minPlayers;
+    }
 
-public int getMaxPlayers() {
-    return maxPlayers;
-}
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
 
-public void setMaxPlayers(int maxPlayers) {
-    this.maxPlayers = maxPlayers;
-}
+    public void setMaxPlayers(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
 
-public String getGameType() {
-    return gameType;
-}
+    public String getGameType() {
+        return gameType;
+    }
 
-public void setGameType(String gameType) {
-    this.gameType = gameType;
-}
+    public void setGameType(String gameType) {
+        this.gameType = gameType;
+    }
 
-public List<Review> getReviews() {
-    return reviews;
-}
+    public List<Review> getReviews() {
+        return reviews;
+    }
 
-public void setReviews(List<Review> reviews) {
-    this.reviews = reviews;
-}
-```
-
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 }
