f05278409130888f86e454557b9d7473c5e27126
vishal vishwakarma <119778329+vishal343012@users.noreply.github.com>
2026-03-13 17:23:16 +0530

    Update Review.java

--- src/main/java/com/javaproject/beans/Review.java
+++ src/main/java/com/javaproject/beans/Review.java
@@ -1,11 +1,35 @@
 package com.javaproject.beans;
 
-import lombok.Data;
-
-@Data
 public class Review {
 
     private Long id;
     private Long gameId;
     private String text;
+
+    public Review() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(Long gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
